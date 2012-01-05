2b4f79ebd9617eb5b080f2ebd87d936fe961a522
nluthy1605@gmail.com <nluthy1605@gmail.com@74d808fd-4bd3-e763-3429-0a81121c8183>
2012-01-05 17:17:35 +0000

    Add layer BUS

---
+++ trunk/Source/QLKS/QLKS.BUS/PhieuThueBUS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLKS.BUS
+{
+    public class PHIEUTHUEBUS
+    {
+        public static bool ThemPhieu(string MPT, string MP, DateTime day, string TT, KHACHHANG[] dsKH)
+        {
+            return PHIEUTHUEDAO.ThemPhieu(MPT, MP, day, TT, dsKH);
+        }
+        public static List<PHIEUTHUE> layDSPT()
+        {
+            return PHIEUTHUEDAO.layDSPT();
+        }
+        public static PHIEUTHUE layPT(string MPT)
+        {
+            return PHIEUTHUEDAO.layPT(MPT);
+        }
+        public static void CapNhat(string MPT, string TT)
+        {
+            PHIEUTHUEDAO.CapNhat(MPT, TT);
+        }
+    }
+}
